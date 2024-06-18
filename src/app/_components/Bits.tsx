26aa485cdf372786398288481f81b4f562655699
Peter Vel <petervel@gmail.com>
2024-06-18 21:01:45 +0200

    Allowing type switching

--- src/app/_components/Bits.tsx
+++ src/app/_components/Bits.tsx
@@ -4,31 +4,72 @@ import { useState } from "react";
 import Bit from "./Bit";
 import css from "./Bits.module.css";
 
+enum ValueType {
+	Hexadecimal = "Hexadecimal",
+	Signed = "Signed",
+	String = "String",
+	Unsigned = "Unsigned",
+}
+
 type BitsProps = {
 	bitCount: number;
 };
 const Bits = (props: BitsProps) => {
 	const { bitCount } = props;
-	const bits = [...Array(bitCount)].map((_, n) => n).reverse();
 
 	const [value, setValue] = useState(5);
 
-	const getMask = (n: number) => 1 << n;
-	const getBit = (n: number) => (value & getMask(n)) != 0;
+	const getBit = (n: number) => (value & (1 << n)) != 0;
 	const setBit = (n: number, val: boolean) => {
-		console.log(n, val);
-		const mask = getMask(n);
+		const mask = 1 << n;
 		setValue((v) => (v & ~mask) | (val ? mask : 0));
 	};
 
+	const [valueType, setValueType] = useState(ValueType.Hexadecimal);
+	const nextType = () => {
+		setValueType((current) => {
+			switch (current) {
+				case ValueType.Hexadecimal:
+					return ValueType.Signed;
+				case ValueType.Signed:
+					return ValueType.String;
+				case ValueType.String:
+					return ValueType.Unsigned;
+				case ValueType.Unsigned:
+					return ValueType.Hexadecimal;
+			}
+		});
+	};
+
+	const getValue = () => {
+		switch (valueType) {
+			case ValueType.String:
+				return `"${String.fromCharCode(value)}" (HEX: 0x${value
+					.toString(16)
+					.toUpperCase()})`;
+			case ValueType.Hexadecimal:
+				return `0x${value.toString(16).toUpperCase()}`;
+			case ValueType.Unsigned:
+				return value;
+			case ValueType.Signed:
+				const mask = 1 << (bitCount - 1);
+				console.log(value, value & mask);
+				return value - (value & mask ? mask << 1 : 0);
+			default:
+				return value;
+		}
+	};
+
+	const bits = [...Array(bitCount)].map((_, n) => n).reverse();
 	return (
 		<div>
-			value: {value}
+			<div>Value: {getValue()}</div>
 			<div className={css.bits}>
 				{bits.map((n) => (
 					<Bit key={n} value={getBit(n)} setValue={(v) => setBit(n, v)} />
 				))}
 			</div>
+			<div onClick={() => nextType()}>{valueType}</div>
 		</div>
 	);
 };
