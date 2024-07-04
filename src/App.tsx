805502180c3c61748c055d3ce922d5ff7c626d73
Peter Vel <petervel@gmail.com>
2024-07-04 23:42:11 +0200

    Got rid of NextJS, straight react now

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Calculator from "./_components/Calculator";
+
+function App() {
+  return (
+    <main>
+      <Calculator />
+    </main>
+  );
+}
+
+export default App;
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Calculator from "./_components/Calculator";
-
-export default function Home() {
-	return (
-		<main>
-			<Calculator />
-		</main>
-	);
-}
